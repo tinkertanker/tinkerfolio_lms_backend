0651fed69b2b53be1f745d4ef5fc4ec9497f335e
michaelchen-lab <michaelchenkaijie2004@gmail.com>
2022-07-18 10:52:35 +0800

    admin password change function

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import *
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_filter = ['user_type']
     search_fields = ['id', 'studentprofile__index', 'studentprofile__assigned_class_code', 'studentprofile__name']
 
