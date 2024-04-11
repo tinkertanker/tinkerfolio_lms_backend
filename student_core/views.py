a219e19f63c28edb53035e90e5bb59f54a76f0f0
ngmh <ngmhcartoon@gmail.com>
2024-04-11 12:11:03 +0800

    edge case when no students

--- student_core/views.py
+++ student_core/views.py
@@ -183,7 +183,10 @@ class EnrollViewSet(viewsets.ViewSet):
             return Response('Student is already in the classroom.', status.HTTP_403_FORBIDDEN)
         present_students = Enroll.objects.filter(classroom=classroom)
         num_of_students = len(present_students)
-        new_index = present_students[present_students.count()-1].studentIndex+1
+        if num_of_students == 0:
+            new_index = 1
+        else:
+            new_index = present_students[present_students.count()-1].studentIndex+1
         classroom.student_indexes = classroom.student_indexes + [new_index]
         classroom.save()
         
